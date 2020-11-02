9850e9a0b9d54e82b9ab6a85e34622a7313e14f0
Karol2302 <karolsz@go2.pl>
2020-11-02 20:48:06 +0100

    Adding application files

---
+++ src/main/java/net/codejava/AppController.java
@@ -0,0 +1,57 @@
+package net.codejava;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class AppController {
+
+	@Autowired
+	private ProduktService service;
+	
+	@RequestMapping("/")
+	public String viewHomePage(Model model) {
+		List<Produkt> listProducts = service.listAll();
+		model.addAttribute("listProducts", listProducts);
+		
+		return "index";
+	}
+	
+	@RequestMapping("/new")
+	public String showNewProductPage(Model model) {
+		Produkt product = new Produkt();
+		model.addAttribute("product", product);
+		
+		return "new_product";
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveProduct(@ModelAttribute("product") Produkt product) {
+		service.save(product);
+		
+		return "redirect:/";
+	}
+	
+	@RequestMapping("/edit/{id}")
+	public ModelAndView showEditProductPage(@PathVariable(name = "id") int id) {
+		ModelAndView mav = new ModelAndView("edit_product");
+		Produkt product = service.get(id);
+		mav.addObject("product", product);
+		
+		return mav;
+	}
+	
+	@RequestMapping("/delete/{id}")
+	public String deleteProduct(@PathVariable(name = "id") int id) {
+		service.delete(id);
+		return "redirect:/";		
+	}
+}
